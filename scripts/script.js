ad119dd1d4a5abfed4552985dd407b2d5605e677
alternateved <alternateved@gmail.com>
2021-02-25 21:35:12 +0100

    Add generateGrid function and style resulting grid

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+const gridContainer = document.querySelector(".container");
+
+function generateGrid(size) {
+  for (let i = 0; i < size; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+
+    for (let j = 0; j < size; j++) {
+      let square = document.createElement("div");
+      square.classList.add("square");
+      row.appendChild(square);
+    }
+    gridContainer.appendChild(row);
+  }
+}
+
+generateGrid(100);
